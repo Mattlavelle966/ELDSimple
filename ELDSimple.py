640277899604eb7a4832c46359169c786b295399
mkirton966 <mkirton966@users.noreply.github.com>
2022-05-24 15:59:09 -0700

    bug patch
    patched the missing 1 required argument error

--- ELDSimple.py
+++ ELDSimple.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import webbrowser
 
 url = "https://www.youtube.com/results?search_query="
@@ -16,21 +17,9 @@ root1["bg"] = "indian red"
 class window(Frame):
     def __init__(self):
 
-        def delete_qg_text(self,e):
-            global search_qg_text
-            self.search_qg.delete(0, END)
-            self.search_qg.insert(0, "")
-
-        def delete_yt_text(self,e):
-            global search_yt_text
-            self.search_yt.delete(0, END)
-            self.search_yt.insert(0, "")
-
         Frame.__init__(self)
-        #self.master.overrideredirect(True)
         self.title_bar = Frame(self, bg="darkgreen", relief="raised", bd=1, padx=6, pady=3)
         self.title_label = Label(self.title_bar, text="ELDSimple", bg="darkgreen", font=("CenturySchoolbookl 10 bold"))
-        #binding title to title_bar
         self.title_bar.bind("<B1-Motion>", move_app)
 
         self.close_label = Label(self.title_bar, text=" X ", bg="darkgreen", fg="white", relief="sunken")
@@ -48,11 +37,9 @@ class window(Frame):
         self.lbl_search2 = LabelFrame(self.lbl, bg="maroon", padx=5, pady=5)
         self.lbl_g_query = LabelFrame(self.lbl, bg="goldenrod", padx=5, pady=5)
         self.lbl_yt_query = LabelFrame(self.lbl, bg="maroon", padx=5, pady=5)
-        self.space3 = Label(self.lbl3, padx=1, pady=140)
         self.search_qg = Entry(self.lbl_g_query, width=50, font=("Arial 20"), bg="indian red", fg="goldenrod")
-        self.search_qg.bind("<Button-1>", delete_qg_text)
         self.search_yt = Entry(self.lbl_yt_query, width=50, font=("Arial 20"), bg="indian red", fg="maroon")
-        self.search_yt.bind("<Button-1>", lambda delete_yt_text: self.search_qg.delete(0, END), self.search_qg.insert(0, ""))
+      
 
 
 
@@ -60,7 +47,7 @@ class window(Frame):
         self.button2 = Button(self.lbl, text="Email", font=('Arial 20'), bg="indian red", command=link_m, padx=339, pady=30)
         self.button3 = Button(self.lbl, text="Email", font=('Arial 20'), bg="indian red", command=link_m2, padx=339, pady=28)
         self.search1 = Button(self.lbl_search1, text="Search", font=('Arial 12'), command=search_query, padx=1, pady=5)
-        self.search2 = Button(self.lbl_search2, text="Search", font=('Arial 12'), command=search_query, padx=1, pady=5)
+        self.search2 = Button(self.lbl_search2, text="Search", font=('Arial 12'), command=search_query2, padx=1, pady=5)
         self.help_b = Button(self.lbl3, text="Help", bg="red4", font=('Arial 11'), command=popup, padx=11, pady=145)
 
         self.title_bar.grid(row=0, column=0, sticky="nw")
@@ -78,7 +65,6 @@ class window(Frame):
         #creates space on grid
         self.space.grid(row=0, column=1)
         self.space2.grid(row=1, column=4)
-        #space3.grid()
         self.lbl.grid(row=2, column=3)
         self.lbl3.grid(rowspan=3, row=1, column=2, columnspan=5)
         self.button1.grid(row=1, column=1)
@@ -98,34 +84,49 @@ def quitter(e):
 def move_app(e):
     root1.geometry(f"+{e.x_root}+{e.y_root}")
 
+def delete_qg_text(e):
+    global search_qg_text
+    root.search_qg.delete(0, END)
+    root.search_qg.insert(0, "")
+    root.search_qg.bind("<Return>", enter_query_qg)
+
+
+def delete_yt_text(e):
+    global search_yt_text
+    root.search_yt.delete(0, END)
+    root.search_yt.insert(0, "")
+    root.search_yt.bind("<Return>", enter_query_yt)
+
 def enter_query_qg(e):
     global url2
-    if self.search_qg.get() == self.search_qg.get():
-        url2 = url2 + self.search_qg.get()
+    if root.search_qg.get() == root.search_qg.get():
+        url2 = url2 + root.search_qg.get()
         webbrowser.open(url2)
 
 def enter_query_yt(e):
     global url
-    if self.search_yt.get() == self.search_yt.get():
-        url = url + self.search_yt.get()
+    if root.search_yt.get() == root.search_yt.get():
+        url = url + root.search_yt.get()
         webbrowser.open(url)
 
 def search_query():
     global url2
-    if self.search_qg.get() == self.search_qg.get():
-        url2 = url2 + self.search_qg.get()
+    if root.search_qg.get() == root.search_qg.get():
+        url2 = url2 + root.search_qg.get()
         webbrowser.open(url2)
 
 def search_query2():
     global url
-    if self.search_yt.get() == self.search_yt.get():
-        url = url + self.search_yt.get()
+    if root.search_yt.get() == root.search_yt.get():
+        url = url + root.search_yt.get()
         webbrowser.open(url)
 
 def link_e():
     webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
+
 def link_m():
     webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
+
 def link_m2():
     webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
 
@@ -135,6 +136,7 @@ def popup():
 
 root = window()
 root.grid()
-
-
+root.close_label.bind("<Button-1>", quitter)
+root.search_qg.bind("<Button-1>", delete_qg_text)
+root.search_yt.bind("<Button-1>", delete_yt_text)
 root1.mainloop()
